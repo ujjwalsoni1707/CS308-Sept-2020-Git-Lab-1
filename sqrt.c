50c7554269e930525fb99df8f9bfcca6dbeb49ab
Ujjwal Soni <ujjwalsoni1707@gmail.com>
2020-10-09 14:53:16 +0530

    Negativesupport

--- sqrt.c
+++ sqrt.c
@@ -16,10 +16,10 @@ int main(int argc, char* argv[]) {
 	int input = atoi(argv[1]);
 	if (input<0) {
 		input= input * (-1);
-		printf("Sqrt of %d is %fi\n",input,sqrt(input));	
+		printf("“NEGSUPPORT: Sqrt of %d is %fi\n",input,sqrt(input));	
 	}
 	else{
-	printf("Sqrt Of %d is %f\n",input,sqrt(input));
+	printf("“NEGSUPPORT: Sqrt of %d is %f\n",input,sqrt(input));
 	}
 	printf("End of program. Exiting");
 	
